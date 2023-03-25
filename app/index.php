d19ef14119b11016e4eb48ad19a6f281daba21bb
Maksym Bielushkin <maxb@getblend.com>
2023-03-25 19:18:25 +0100

    Addded cover image and template

---
+++ app/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>PHP course for beginners in 2023 year</title>
+    <meta charset="utf-8">
+    <style>
+        h1 {
+            text-align: center;
+        }
+
+        p {
+            text-align: center;
+        }
+
+        div {
+            text-align: center;
+        }
+
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+
+        ul.a {
+            list-style-type: circle;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Coding with Max Belushkin</h1>
+    <p>Welcome to PHP course for beginners in 2023 year.</p>
+    <div><img src="./img/cover.png" width="20%" height="20%"></div>
+    <div><?php echo "Зміст:"; ?></div>
+    <br />
+    <div>
+        <ul>
+            <li><a target="_blank" href="/step1.php">Базовий синтаксис</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
